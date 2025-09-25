0fc60f0064c31dc82a71bfe57a42fb760937cbdc
Daniel Ernest <chinedu.daniel.ernest@gmail.com>
2025-09-25 18:12:39 +0100

    first commit

---
+++ .vscode/app.js
@@ -0,0 +1,75 @@
+// let colors = ["Apple", "Mango","Corn"]
+// colors.pop()
+// console.log(colors)
+
+// "div overflow-x:auto;" is used for making a table in css responsive
+
+// function you1(){
+//   console.log('My name is Ernest Daniel Chinedu');
+//   console.log('2008')
+//   console.log(9+9)
+// }
+
+// you1();
+// you1();
+
+// const bio = {
+//   Name: 'Ernest Daniel',
+//   Age: 17,
+//   Dateofbirth: 'August 18 2008', 
+//   Profession: 'Software Developer',
+//   data:{
+//     Class: 'Done with secondary schoool',
+//     Religion: 'Christian'
+//   }
+// }
+//   bio.data.Religion = 'Muslim'
+
+// console.log(bio)
+
+
+//  console.log(54 > 4 + 49)
+
+// let  age = 14;
+// const drivingAge = 18
+
+/*if (age >= drivingAge) {
+   console.log('You can drive.')
+   console.log('Congrats')
+}else if (age >= drivingAge) {
+   console.log('Almost there')
+} else {
+   console.log('You cannot drive.')
+   console.log('Beware of 419.')
+}*/
+
+
+/*let products = {
+   Name: 'Bread',
+   Price: '500',
+   Manufacturer: 'Ernest daniel chinedu.',
+}
+console.log(products)
+console.log(products.Price.Manufacturer)
+products.Name = 'banana'
+console.log(products)
+products.list = '300'
+console.log(products)
+delete products.list;
+console.log(products)
+console.log(typeof products)
+*/
+
+// const schoool = {
+//    ['Class-School']: 'Primary 5'
+// };
+// console.log(schoool)
+// console.log(schoool['Class-School'])
+
+// console.log(typeof Math.random)
+
+// console.log('My name is daniel ernest'.length)
+// console.log('The name of my father is'.toLowerCase())
+
+// console.log(true.to)
+
